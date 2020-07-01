7b307e28938296594f6071e9ab969b17916a68ff
Luis Bonilla <luisbonilla345@gmail.com>
2020-07-01 14:54:45 -0600

    definiciones de rutas de paginas institucionales

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('name', function () {
+    return 'Plataforma Digital de Noticas y Reseñas de Tecnología';
+});
+
+Route::get('creador', function () {
+    return 'Luis Manuel Bonilla Anaya';
+});
+
+Route::get('porque', function () {
+    return 'Además que pondré en practica mis conocimientos durante el curso, 
+            me gusta bastante la idea de crear esta plataforma que trate acerca 
+            de la tecnología, un mundo con muchas personas geek interesadas y posibles usuarios de esta';
+});
+
+Route::get('objetivo', function () {
+    return 'Crear un plataforma funciona de diferentes tipos de artículos relacionado al mundo de la tecnología.';
+});
+
+Route::get('usuariofinal', function () {
+    return 'Personas que están interesadas en conocer las noticias más recientes del mundo tecnológico';
+});
